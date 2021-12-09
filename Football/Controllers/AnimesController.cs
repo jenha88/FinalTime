bbee06ceb0d6bca5f56d9342863f4ec03444316f
Jennifer Ha <Jennifer.Ha-1@ou.edu>
2021-12-08 21:50:12 -0600

    commiting things

---
+++ Football/Controllers/AnimesController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Football.Models;
+
+namespace Football.Controllers
+{
+    public class AnimesController : Controller
+    {
+        private DB_128040_practiceEntities db = new DB_128040_practiceEntities();
+
+        // GET: Animes
+        public ActionResult Index()
+        {
+            return View(db.Animes.ToList());
+        }
+
+        // GET: Animes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Anime anime = db.Animes.Find(id);
+            if (anime == null)
+            {
+                return HttpNotFound();
+            }
+            return View(anime);
+        }
+
+        public ActionResult Rank(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Anime anime = db.Animes.Find(id);
+            if (anime == null)
+            {
+                return HttpNotFound();
+            }
+            return View(anime);
+        }
+
+        // GET: Animes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Animes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "anime_id,name,genre,type,episodes,rating,members")] Anime anime)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Animes.Add(anime);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(anime);
+        }
+
+        // GET: Animes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Anime anime = db.Animes.Find(id);
+            if (anime == null)
+            {
+                return HttpNotFound();
+            }
+            return View(anime);
+        }
+
+        // POST: Animes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "anime_id,name,genre,type,episodes,rating,members")] Anime anime)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(anime).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(anime);
+        }
+
+        // GET: Animes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Anime anime = db.Animes.Find(id);
+            if (anime == null)
+            {
+                return HttpNotFound();
+            }
+            return View(anime);
+        }
+
+        // POST: Animes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Anime anime = db.Animes.Find(id);
+            db.Animes.Remove(anime);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Football/Controllers/SpotifiesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Football.Models;
+
+namespace Football.Controllers
+{
+    public class SpotifiesController : Controller
+    {
+        private DB_128040_practiceEntities db = new DB_128040_practiceEntities();
+
+        // GET: Spotifies
+        public ActionResult Index()
+        {
+            return View(db.Spotifies.ToList());
+        }
+
+        // GET: Spotifies/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Spotify spotify = db.Spotifies.Find(id);
+            if (spotify == null)
+            {
+                return HttpNotFound();
+            }
+            return View(spotify);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
