b0cf677f891152a2332cbbed97a4e7f5f4819637
Jennifer Ha <Jennifer.Ha-1@ou.edu>
2021-12-11 01:03:15 -0600

    commiting things

---
+++ EFPractice/Controllers/MoviesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EFPractice.Models;
+
+namespace EFPractice.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private DB_128040_practiceEntities db = new DB_128040_practiceEntities();
+
+        // GET: Movies
+        public ActionResult Index()
+        {
+           
+            return View(db.Movies.ToList());
+        }
+
+        // GET: Movies/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ EFPractice/Controllers/TopController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EFPractice.Models;
+
+namespace EFPractice.Controllers
+{
+    public class TopController : Controller
+    {
+        private DB_128040_practiceEntities db = new DB_128040_practiceEntities();
+
+        // GET: Top
+        public ActionResult Index()
+        {
+            var movies = db.Movies.OrderByDescending(m => m.imdb_score).Take(10);
+            return View(movies);
+        }
+
+        // GET: Top/Details/5
+        public ActionResult Movies(int? id)
+        {
+            if (id == null)
+            {
+                var mv= db.Movies.OrderByDescending(m => m.imdb_score).Take(10);
+                return View(mv);
+            }
+            var movies = db.Movies.OrderByDescending(m => m.imdb_score).Take((int)id);
+            if (movies == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movies);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
